655005171017c0a5be6731a7aa9e806e193ee6b8
Morne Venter <morne@Mornes-MacBook-Pro.local>
2024-06-01 13:52:10 +0200

    add gdscript and 4 tab indent

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -1,7 +1,15 @@
 ---@type ChadrcConfig
 local M = {}
+local opt = vim.opt
 
 M.ui = { theme = 'catppuccin' }
 M.plugins = 'custom.plugins'
+
+-- Indenting
+opt.expandtab = false
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.tabstop = 4
+opt.softtabstop = 4
 
 return M
--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local on_attach = configs.on_attach
 local capabilities = configs.capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "tsserver", "eslint" }
+local servers = { "tsserver", "eslint", "gdscript"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
