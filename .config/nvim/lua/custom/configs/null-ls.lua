b35c302a3eb555f88bbc316cb581d185aa02beea
Morne Venter <morne@Mornes-MacBook-Pro.local>
2024-06-02 19:49:54 +0200

    update spacing

--- .config/nvim/lua/custom/configs/null-ls.lua
+++ .config/nvim/lua/custom/configs/null-ls.lua
@@ -4,15 +4,15 @@ local formatting = null_ls.builtins.formatting
 local lint = null_ls.builtins.diagnostics
 
 local sources = {
-  formatting.prettier,
-  formatting.markdownlint,
-  formatting.gdformat,
-  formatting.csharpier,
-  formatting.yq.with({filetypes = {"csv"}}),
-  lint.shellcheck
+	formatting.prettier,
+	formatting.markdownlint,
+	formatting.gdformat,
+	formatting.csharpier,
+	formatting.yq.with({ filetypes = { "csv" } }),
+	lint.shellcheck
 }
 
 null_ls.setup {
-   debug = true,
-   sources = sources,
+	debug = true,
+	sources = sources,
 }
--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -1,35 +1,35 @@
 local plugins = {
-  {
-    "neovim/nvim-lspconfig",
-     dependencies = {
-       "jose-elias-alvarez/null-ls.nvim",
-       config = function()
-         require "custom.configs.null-ls"
-       end,
-     },
-     config = function()
-        require "plugins.configs.lspconfig"
-        require "custom.configs.lspconfig"
-     end
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = {
-      filters = {
-        custom = {
-          "^.git$", -- .git ignore
-          "^.DS_Store$", -- Mac specific ignore
-          "\\.import$", -- Godot import ignore
-        }
-      },
-      view = {
-        adaptive_size = true,
-        width = 40,
-      },
-    },
-  },
-  {
-    "Hoffs/omnisharp-extended-lsp.nvim"
-  }
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"jose-elias-alvarez/null-ls.nvim",
+			config = function()
+				require "custom.configs.null-ls"
+			end,
+		},
+		config = function()
+			require "plugins.configs.lspconfig"
+			require "custom.configs.lspconfig"
+		end
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = {
+			filters = {
+				custom = {
+					"^.git$", -- .git ignore
+					"^.DS_Store$", -- Mac specific ignore
+					"\\.import$", -- Godot import ignore
+				}
+			},
+			view = {
+				adaptive_size = true,
+				width = 40,
+			},
+		},
+	},
+	{
+		"Hoffs/omnisharp-extended-lsp.nvim"
+	},
 }
 return plugins
